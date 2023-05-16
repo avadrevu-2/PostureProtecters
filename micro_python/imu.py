d61d124474c2de2cf99028cc376dcfc7e3c20c92
Abhijit Vadrevu <avadrevu@ucsd.edu>
2023-05-15 18:16:40 -0700

    imu

--- micro_python/imu.py
+++ micro_python/imu.py
@@ -4,10 +4,9 @@ from machine import sleep
 import mpu6050
 
 
-i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
-#i2c = I2C(scl=Pin(5), sda=Pin(4))       #initializing the I2C method for ESP8266
+i2c = I2C(scl=Pin(22), sda=Pin(23))     #initializing the I2C method for ESP32
 mpu= mpu6050.accel(i2c)
 while True:
- mpu.get_values()
- print(mpu.get_values())
- sleep(500)
+    mpu.get_values()
+    print(mpu.get_values())
+    sleep(500)
